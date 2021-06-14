c602331ebee441cb150b49e409f3508bb20578d0
psyduck777 <email1@virkkunen.email>
2021-06-14 17:50:17 +0300

    Fixed Auth To Show Adress instead of balance

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -2,16 +2,15 @@ import { Signer } from "@waves/signer";
 import { ProviderWeb } from "@waves.exchange/provider-web";
 
 async function authFunc() {
-  const signer = new Signer({
-    // Specify URL of the node on Testnet
-    NODE_URL: "https://nodes-testnet.wavesnodes.com",
-  });
-  signer.setProvider(new ProviderWeb("https://testnet.waves.exchange/signer/"));
+	const signer = new Signer({
+		// Specify URL of the node on Testnet
+		NODE_URL: "https://nodes-testnet.wavesnodes.com",
+	});
+	signer.setProvider(new ProviderWeb("https://testnet.waves.exchange/signer/"));
 
-  const user = await signer.login();
-  const balances = await signer.getBalance();
-  console.log(balances);
-  alert("My balance is " + balances[0].amount)
+	const user = await signer.login();
+
+	alert("My address is " + user.address);
 }
 
 export default authFunc;
